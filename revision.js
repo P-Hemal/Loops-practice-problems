8252a6f7417de29f4953f8d23aa15e740217bb1d
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-02-25 12:28:24 +0600

    else updated

--- revision.js
+++ revision.js
@@ -14,3 +14,19 @@ var item3 = tableItem[3];
 console.log(item3);
 tableItem[4] = 'speaker';
 console.log(tableItem);
+var penIndex = tableItem.indexOf('pen');
+console.log(penIndex);
+tableItem.push ('tape');
+console.log(tableItem);
+tableItem.pop [2];
+console.log(tableItem);
+
+if(tableItem.length > 6){
+    console.log('clean kora lagbe')
+}
+else if (tableItem[2] == 'mobile'){
+    console.log('mobile rekhe porte boso')
+}
+else{
+    console.log('clean e ache onek')
+}
