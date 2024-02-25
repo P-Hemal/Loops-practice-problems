e3dc39e7552bbe28c6900c250ad03654c7549ce2
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-02-25 12:00:03 +0600

    revision

--- revision.js
+++ revision.js
@@ -1,1 +1,16 @@
-var bottle = 'water'
+// string type variable
+
+var bottle = 'water';
+var bottleColor = 'yellow';
+var penColor = 'blue';
+// number type variable
+var sunglassPrice = 120;
+// boolean type variable
+var isExpensive = true;
+
+
+var tableItem = ['bootle', 'pen', 'mouse', 'mobile', 'watch'];
+var item3 = tableItem[3];
+console.log(item3);
+tableItem[4] = 'speaker';
+console.log(tableItem);
