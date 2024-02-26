9964f8e2c1939471771178c3b60b79948918ee03
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-02-26 12:10:59 +0600

    updated

---
+++ for.js
@@ -0,0 +1,11 @@
+// //While loop
+// var number = 1;
+// while(number < 10){
+//     console.log(number);
+//     number++;
+// }
+
+//for loop
+
+for(var number = 1; number < 10; number++)
+console.log('call me please');
