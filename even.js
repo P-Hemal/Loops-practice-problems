3eec4fa6e0fb3116e84b4ce4d77cc28f6cb6d46e
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-02-26 10:14:05 +0600

    even and odd loop

---
+++ even.js
@@ -0,0 +1,14 @@
+// verify even numbers
+
+var number = 0;
+while(number <= 10){
+    console.log(number);
+    number = number + 2;
+}
+
+// verify odd numbers
+var number = 1;
+while(number <= 10){
+    console.log(number);
+    number = number + 2;
+}
--- while.js
+++ while.js
@@ -1,6 +1,7 @@
 var num = 0;
 while(num < 5){
     console.log('Hello !!');
-    console.log(num);
+    
     num++;
 }
+console.log(num);
