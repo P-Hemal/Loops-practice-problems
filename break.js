ea86aad695e1fdc44f1c8a5e1b6f48877ce950d4
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-02-27 11:48:15 +0600

    break

---
+++ break.js
@@ -0,0 +1,21 @@
+// for(i = 1; i < 20; i++){
+//     if(i > 10)
+//     break;
+// console.log(i)
+// }
+
+// var numbers = 0;
+// while(numbers < 10){
+//     console.log('run machine')
+//     numbers++
+// }
+
+var items = ['bootle', 'mouse', 'mobile', 'pen', 'watch'];
+for(i = 0; i < items.length; i++){
+    var item = items[i];
+    if(item == 'pen'){
+        break;
+    }
+    console.log(item)
+    
+}
