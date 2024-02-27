c3914aa171285b8d53c8b428f4fc080e9c6c22e4
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-02-27 12:46:36 +0600

    expenses

---
+++ apple-orange.js
@@ -0,0 +1,7 @@
+
+var moneyGiven = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+var totalPrice = (applePrice + orangePrice);
+var returnMoney = (moneyGiven - totalPrice);
+console.log(returnMoney)
--- while.js
+++ while.js
@@ -1,7 +1,6 @@
 var num = 0;
 while(num < 5){
-    console.log('Hello !!');
+    console.log(num);
     
     num++;
 }
-console.log(num);
