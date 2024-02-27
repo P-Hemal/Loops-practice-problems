18a53f55e4a6a029fdf6f7837e11cb49fdc3542d
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-02-27 11:23:57 +0600

    loop

---
+++ array-loop.js
@@ -0,0 +1,5 @@
+var numbers = [ 10, 25, 45, 87, 95, 51, 68]
+for(var i = 0; i < 7; i++){
+    var number = numbers[i];
+    console.log(number);
+}
--- for.js
+++ for.js
@@ -7,5 +7,10 @@
 
 //for loop
 
-for(var number = 1; number < 10; number++)
-console.log('call me please');
+// for(var number = 1; number < 10; number++)
+// console.log('call me please');
+
+
+for(var i = 0; i <= 10; i+=2){
+    console.log(i)
+}
