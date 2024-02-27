79d6f07db57c55fe54661ccda27de0a2d789b6f2
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-02-27 13:01:33 +0600

    average

---
+++ average.js
@@ -0,0 +1,9 @@
+var mathMarks = 75.25;
+var bioMarks = 65;
+var chemistryMarks = 80;
+var physicsMarks = 35.45;
+var banMarks = 99.50;
+var totalMarks = mathMarks + bioMarks + chemistryMarks + physicsMarks + banMarks;
+var average = totalMarks / 5;
+var averageTwoDecimal = average.toFixed(2);
+console.log(averageTwoDecimal)
