49641071388081c88f734919738ded3ea5563ae1
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-02-28 10:09:12 +0600

    updated

---
+++ grading.js
@@ -0,0 +1,38 @@
+var marks = 85;
+if(marks >= 80){
+    console.log('A Grade')
+}
+else if(marks >= 60){
+    console.log('B Grade')
+}
+else if(marks >= 50){
+    console.log('C Grade')
+}
+else if(marks >= 40){
+    console.log('D Grade')
+}
+else if(marks <= 39){
+    console.log('F Grade')
+}
+else{
+    console.log('invalid')
+}
+var tomMarks = 66;
+if(tomMarks >= 80){
+    console.log('Tom = A Grade')
+}
+else if(tomMarks >= 60){
+    console.log('Tom = B Grade')
+}
+else if(tomMarks >= 50){
+    console.log('Tom = C Grade')
+}
+else if(tomMarks >= 40){
+    console.log('Tom = D Grade')
+}
+else if(tomMarks <= 39){
+    console.log('Tom = F Grade')
+}
+else{
+    console.log('invalid')
+}
