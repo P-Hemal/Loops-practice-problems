9b2a16effb818675a2fe50eea01b8e29a3342105
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-02-29 10:23:45 +0600

    continue

--- continue.js
+++ continue.js
@@ -1,7 +1,7 @@
-var numbers = [ 10, 25, 45, 87, 95, 51, 68, 71, 82, 77, 50]
-for(var i = 0; i < numbers.length; i++){
+var numbers = [ 10, 25, 45, 87, 95, 51, 68, 71, 82, 77, 50, 150, 175, 200, 230, 181, 248, 261]
+for(var i = 1; i < numbers.length; i++){
     var number = numbers[i];
-    if(number > 80){
+    if(number >= 200){
         continue;
     }
     console.log(number);
---
+++ practice.js
@@ -0,0 +1,38 @@
+// Task 1
+// var num = 1;
+// while(num <= 39){
+// console.log('abdul hakim hemal')
+// num++
+// }
+
+//  Task 2
+// // var num = 58;
+// //  while(num <= 98){
+// //     console.log(num);
+// //     num++
+// //  }
+
+// // Task 3
+// var num = 412;
+// while(num <= 456){
+// console.log(num)
+// num = num + 2;
+// }
+
+// // Task 4
+// var num = 581;
+// while(num <= 623){
+// console.log(num)
+// num = num + 2;
+// }
+
+
+var items = ['bootle', 'mouse', 'mobile', 'pen', 'watch'];
+for(i = 0; i < items.length; i++){
+    var item = items[i];
+    // if(item == 'pen'){
+    //     break;
+    // }
+    console.log(item)
+    
+}
